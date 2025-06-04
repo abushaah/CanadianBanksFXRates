fc232ac8187f5444275a7382460f82ea1665134e
Haifaa Abushaaban <haifaa.abushaaban@hotmail.com>
2025-06-04 00:00:54 -0400

    saving progress; add api calls

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,10 @@ const { Pool } = require('pg');
 const app = express();
 const port = 5000;
 
+// These are constants set for the RBC API
+const BASE_CURRENCY = 'CAD';
+const AMOUNT = 100;
+
 const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -22,12 +26,56 @@ app.get('/test-db', async (req, res) => {
   }
 });
 
-app.get('/api', async (req, res) => {
+app.get('/bmo', async (req, res) => {
   try {
-    const response = await axios.get('https://api.github.com');
+    const response = await axios.get('https://www.bmo.com/bmocda/templates/json_fx_include.jsp');
+    res.json({ data: response });
+  } catch (err) {
+    res.status(500).json({ error: `Failed to fetch from BMO ${err}` });
+  }
+});
+
+app.get('/scotia', async (req, res) => {
+  try {
+    const response = await axios.get('https://dmtsms.scotiabank.com/api/rates/fxr');
     res.json({ data: response.data });
   } catch (err) {
-    res.status(500).json({ error: 'Failed to fetch from GitHub API' });
+    res.status(500).json({ error: `Failed to fetch from Scotia ${err}` });
+  }
+});
+
+app.get('/rbc', async (req, res) => {
+  try {
+    const currency = await axios.get('https://apps.royalbank.com/apps/foreign-exchange-calculator/api/rates/currencies?do=currencies');
+
+    const results = [];
+    const trades = new Set([...currency.buy, ...currency.sell]);
+
+    for (const code of trades) {
+      try {
+        const response = await axios.get('https://apps.royalbank.com/apps/foreign-exchange-calculator/api/rates/currencies', {
+          params: {
+            do: 'conv',
+            from: BASE_CURRENCY,
+            to: code,
+            trade: 'sell',
+            amount: AMOUNT
+          }
+        });
+
+        const { frate, trate } = response.data;
+        results.push({
+          CURRENCY_CODE: code,
+          CLIENT_BUY: frate,
+          CLIENT_SELL: trate
+        });
+      } catch (err) {
+        console.error(`Failed for ${code}:`, err.message);
+      }
+    }
+    res.json({ data: trades });
+  } catch (err) {
+    res.status(500).json({ error: `Failed to fetch from RBC ${err}` });
   }
 });
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,22 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [bmoData, setBMOData] = useState(null);
+  const [scotiaData, setScotiaData] = useState(null);
+  const [rbcData, setRBCData] = useState(null);
   const [db, setDb] = useState(null);
 
   useEffect(() => {
-    axios.get('/api')
-      .then(res => setData(res.data))
+    axios.get('/bmo')
+      .then(res => setBMOData(res.data))
+      .catch(err => console.error(err));
+
+    axios.get('/scotia')
+      .then(res => setScotiaData(res.data))
+      .catch(err => console.error(err));
+
+    axios.get('/rbc')
+      .then(res => setRBCData(res.data))
       .catch(err => console.error(err));
 
     axios.get('/test-db')
@@ -18,9 +28,14 @@ function App() {
   return (
     <div>
       <h1>Frontend (React)</h1>
-      <p>Testing API calls from backend:</p>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-      <p>Testing database from backend:</p>
+      <h2>Testing API calls from backend:</h2>
+      <h3>BMO</h3>
+      <pre>{JSON.stringify(bmoData, null, 2)}</pre>
+      <h3>Scotia</h3>
+      <pre>{JSON.stringify(scotiaData, null, 2)}</pre>
+      <h3>RBC</h3>
+      <pre>{JSON.stringify(rbcData, null, 2)}</pre>
+      <h2>Testing database from backend:</h2>
       <pre>{JSON.stringify(db, null, 2)}</pre>
     </div>
   );
